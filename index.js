6eff091e325c43fd5d92a326b1284fef16b7eeff
Yeefs <SeedsAreTasty@outlook.com>
2021-12-11 00:28:05 -0600

    Pixi.js added
    First Pixi.js inclusion

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const http = require('http')
 const server = http.Server(app)
 //const server = http.createServer(app)
 
+let people = {};
+
 const io = require('socket.io')(server, { cors: { origin: "*" }})
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -17,14 +19,28 @@ app.use(express.static(path.join(__dirname, 'public')))
 server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 io.on('connection', (socket) => {
+  
   console.log("User connected: " + socket.id);
+
+  socket.on("profile/join", function(username) {
+    console.log("SOMEBODY JOINED");
+    people[socket.id] = username;
+    socket.emit("info/message", "You have connected.");
+    io.emit("info/chatmessage", username + " has connected");
+  });
+
+  socket.on("disconnect", function() {
+    io.emit("info/chatmessage", people[socket.id] + " has left.");
+    delete people[socket.id];
+  })
+
   socket.on('message', function(msg) {
     io.emit('message', "Guest", new Date().toTimeString(), msg);
   })
+
 });
 
 //setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 
-app.get('/', (req, res) => res.render('pages/index'));
-app.get('/chat', (req, res) => res.render('pages/chat'));
+app.get('/', (req, res) => res.render('pages/app.ejs'));
 app.get('/credits', (req, res) => res.render('pages/credits'));
