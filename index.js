0a5826175c9fa50a3bce34b3b04ecc40e45b4361
Yeefs <SeedsAreTasty@outlook.com>
2021-12-12 03:23:57 -0600

    Temporarily Finished
    Added UI, Design, Logic
    
    Added Everything

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 
 const http = require('http')
 const server = http.Server(app)
-const personSize = 30
+const personSize = 40
 //const server = http.createServer(app)
 
 let people = {};
@@ -14,10 +14,10 @@ let people = {};
 //Region Input
 
 function region(x1, y1, x2, y2, name) {
-  this.x1 = x1;
-  this.y1 = y1;
-  this.x2 = x2;
-  this.y2 = y2;
+  this.x1 = parseInt(x1);
+  this.y1 = parseInt(y1);
+  this.x2 = parseInt(x2);
+  this.y2 = parseInt(y2);
   this.width = x2-x1;
   this.height = y2-y1;
   this.name = name;
@@ -34,12 +34,12 @@ region.prototype.toString = function() {
 //Width and Height parameters are in person-sized units
 
 function table(w, h, x, y, name) {
-  this.width = w;
-  this.height = h;
-  this.x = x;
-  this.y = y;
+  this.width = parseInt(w) * personSize;
+  this.height = parseInt(h) * personSize;
+  this.x = parseInt(x);
+  this.y = parseInt(y);
   this.name = name;
-  this.region = new region(x-personSize, y-personSize, x + (w + 1) * personSize, y + (h + 1) * personSize, name + "TableRegion");
+  this.region = new region(this.x-personSize, this.y-personSize, this.x + (this.width + personSize), this.y + (this.height + personSize), name + "TableRegion");
 }
 
 table.prototype.toString = function() {
@@ -47,11 +47,11 @@ table.prototype.toString = function() {
 }
 
 
-let tl = new table(3, 2, 60, 60, "TLT");
-let tr = new table(3, 2, 300, 60, "TRT");
-let bl = new table(3, 2, 60, 180, "BLT");
-let br = new table(3, 2, 300, 180, "BRT");
-let sr = new region(180, 0, 270, 30, "SR");
+let tl = new table(3, 2, personSize*3, personSize*3, "TLT");
+let tr = new table(3, 2, personSize*11, personSize*3, "TRT");
+let bl = new table(3, 2, personSize*3, personSize*7, "BLT");
+let br = new table(3, 2, personSize*11, personSize*7, "BRT");
+let sr = new region(personSize*7, personSize, personSize*10, personSize*2, "SRegion");
 
 let tables = [
   tl.toString(),
@@ -74,28 +74,28 @@ io.on('connection', (socket) => {
   console.log("User connected: " + socket.id);
 
   socket.on("profile/join", function(username, x, y, region) {
-    console.log("SOMEBODY JOINED");
+    console.log("A user has joined.");
     //Double Checking to make sure one socket cannot create more than one student
     if (people[socket.id] != undefined) {
       io.emit("info/removePlayer", socket.id);
     }
-    //FORMAT: Username, Current X, Current Y
-    people[socket.id] = [username, x, y, region];
-    socket.emit("info/message", "You have connected.");
-    io.emit("info/chatmessage", username + " has connected");
+    //FORMAT: Username, Current X, Current Y, Region, Seat X, Seat Y
+    people[socket.id] = [username, x, y, region, null, null];
+    socket.emit("info/message", "[INFO]", "You have connected.");
+    io.emit("info/message", "[INFO]", username + " has connected.");
     io.emit("info/addPlayer", socket.id, people[socket.id]);
   });
 
   socket.on("disconnect", function() {
     if (people[socket.id] != undefined) {
-      io.emit("info/chatmessage", people[socket.id][0] + " has left.");
+      io.emit("info/message", "[INFO]", people[socket.id][0] + " has disconnected.");
       io.emit("info/removePlayer", socket.id);
       delete people[socket.id];
     }
   });
 
   socket.on("info/getPlayerList", function() {
-    console.log("GETTING");
+    console.log("Sending Player Info...");
     socket.emit("info/playerList", people);
   });
 
@@ -109,8 +109,56 @@ io.on('connection', (socket) => {
     io.emit("movement/update", socket.id, people[socket.id]);
   });
 
-  socket.on('message', function(msg) {
-    io.emit('message', "Guest", new Date().toTimeString(), msg);
+  socket.on("movement/requestSit", function (x, y) {
+    let ret = true;
+    for (let p in people) {
+      if (people[p][4] == x && people[p][5] == y) {
+        ret = false;
+      }
+    }
+    if (ret == true) {
+      people[socket.id][4] = x;
+      people[socket.id][5] = y;
+      socket.emit("movement/sitAccept", x, y);
+    }
+  });
+
+  socket.on("movement/unsit", function() {
+    people[socket.id][4] = null;
+    people[socket.id][5] = null;
+  });
+
+  socket.on("info/setRegion", function(region) {
+    if (people[socket.id][3] != region) {
+      if (people[socket.id][3] != null) {
+        io.emit("info/regionMessage", people[socket.id][3], "[INFO]", `${people[socket.id][0]} has left your table.`);
+      }
+      people[socket.id][3] = region;
+      if (region != null) {
+        io.emit("info/regionMessage", region, "[INFO]", `${people[socket.id][0]} has joined table ${people[socket.id][3]}.`);
+      }
+    }
+  });
+
+  socket.on('info/sendMessage', function(scope, msg) {
+    switch (scope) {
+      case "region":
+        if (people[socket.id][3] == null) {
+          socket.emit("info/message", "[INFO]", "You cannot send a table message when you are not at a table");
+          break;
+        }
+        if (people[socket.id][3] == "SRegion") {
+          io.emit("info/message", `[PRESENTER] ${people[socket.id][0]}`, msg);
+          break;
+        }
+        io.emit("info/regionMessage", people[socket.id][3], people[socket.id][0], msg);
+        break;
+        case "global":
+          io.emit("info/message", people[socket.id][0], msg);
+        break;
+        default:
+          socket.emit("info/message", "[INFO]", "Invalid message scope.");
+    }
   });
 
 });
