50986399c28e89bc2d209e47f2fce2f1ab791416
Yeefs <SeedsAreTasty@outlook.com>
2021-12-12 11:17:06 -0600

    Clean Up 1
    Added comments to the code

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Standard Server Constants
+
 const path = require('path')
 
 const PORT = process.env.PORT || 5000
@@ -6,13 +8,18 @@ const app = express()
 
 const http = require('http')
 const server = http.Server(app)
-const personSize = 40
-//const server = http.createServer(app)
 
+//Pixel size of a student
+const personSize = 40
+
+//List of people currently logged in, as well as information stored in the server about them
 let people = {};
 
-//Region Input
 
+
+//Table Functions
+
+//This creates a region object depending on two corners (points) and a name
 function region(x1, y1, x2, y2, name) {
   this.x1 = parseInt(x1);
   this.y1 = parseInt(y1);
@@ -23,16 +30,19 @@ function region(x1, y1, x2, y2, name) {
   this.name = name;
 }
 
+//Checks if a point is within the region
+
 region.prototype.isIn = function(x, y) {
   return ((Math.sign(this.x1 - x) != Math.sign(this.x2 - x)) && (Math.sign(this.y1 - y) != Math.sign(this.y2 - y)));
 }
+
+//Format for distributing region to the clients
 
 region.prototype.toString = function() {
   return `R: [${this.x1},${this.y1},${this.x2},${this.y2},${this.name}]`;
 }
 
-//Width and Height parameters are in person-sized units
-
+//Creates a tabkes depending on the (Width and Height parameters) are in person-sized units
 function table(w, h, x, y, name) {
   this.width = parseInt(w) * personSize;
   this.height = parseInt(h) * personSize;
@@ -42,10 +52,13 @@ function table(w, h, x, y, name) {
   this.region = new region(this.x-personSize, this.y-personSize, this.x + (this.width + personSize), this.y + (this.height + personSize), name + "TableRegion");
 }
 
+//Format for distributing tables to the clients
+
 table.prototype.toString = function() {
   return `T: [${this.width},${this.height},${this.x},${this.y},${this.name}]`;
 }
 
+//Table/Region Constants that create the room
 
 let tl = new table(3, 2, personSize*3, personSize*3, "TLT");
 let tr = new table(3, 2, personSize*11, personSize*3, "TRT");
@@ -61,6 +74,8 @@ let tables = [
   sr.toString()
 ];
 
+//Socket.io and Express Setup
+
 const io = require('socket.io')(server, { cors: { origin: "*" }})
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -69,24 +84,38 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
+//Socket.io Events
+
 io.on('connection', (socket) => {
   
+  //On connection
   console.log("User connected: " + socket.id);
 
+
+  //On signing into the classroom with a username
   socket.on("profile/join", function(username, x, y, region) {
+
     console.log("A user has joined.");
+
     //Double Checking to make sure one socket cannot create more than one student
     if (people[socket.id] != undefined) {
       io.emit("info/removePlayer", socket.id);
     }
+
+    //Initalizes the profile stored by the server identified by your socket id 
     //FORMAT: Username, Current X, Current Y, Region, Seat X, Seat Y
     people[socket.id] = [username, x, y, region, null, null];
+
+    //Updating individual clients that someone has joined
     socket.emit("info/message", "[INFO]", "You have connected.");
     io.emit("info/message", "[INFO]", username + " has connected.");
     io.emit("info/addPlayer", socket.id, people[socket.id]);
   });
 
+  //On disconnect of a user
   socket.on("disconnect", function() {
+    
+    //Updating both client/server side that someone has left
     if (people[socket.id] != undefined) {
       io.emit("info/message", "[INFO]", people[socket.id][0] + " has disconnected.");
       io.emit("info/removePlayer", socket.id);
@@ -94,28 +123,36 @@ io.on('connection', (socket) => {
     }
   });
 
+  //Return the profiles stored if needed by the client
   socket.on("info/getPlayerList", function() {
     console.log("Sending Player Info...");
     socket.emit("info/playerList", people);
   });
 
+  //Returns the objects necessary to create the classroom
   socket.on("info/getObjects", function() {
     socket.emit('info/objectList', tables);
   });
 
+  //Updates the server-side position if a client sends a movement event and updates other clients about this movement event. 
   socket.on("movement/goto", function(x, y) {
     people[socket.id][1] = x;
     people[socket.id][2] = y;
     io.emit("movement/update", socket.id, people[socket.id]);
   });
 
+  //Checks to see upon a sitting request to see if someone else is sitting or not
   socket.on("movement/requestSit", function (x, y) {
+
+    //Sitting check
     let ret = true;
     for (let p in people) {
       if (people[p][4] == x && people[p][5] == y) {
         ret = false;
       }
     }
+
+    //If the position is a valid seat not taken up by someone else, it returns a signal back to the client accepting its seating request.
     if (ret == true) {
       people[socket.id][4] = x;
       people[socket.id][5] = y;
@@ -123,12 +160,17 @@ io.on('connection', (socket) => {
     }
   });
 
+
+  //Client updates server about when it stops sitting
   socket.on("movement/unsit", function() {
     people[socket.id][4] = null;
     people[socket.id][5] = null;
   });
 
+  //Client updates server about what region it is currently in
   socket.on("info/setRegion", function(region) {
+
+    //Updates other clients about the region change
     if (people[socket.id][3] != region) {
       if (people[socket.id][3] != null) {
         io.emit("info/regionMessage", people[socket.id][3], "[INFO]", `${people[socket.id][0]} has left your table.`);
@@ -140,8 +182,10 @@ io.on('connection', (socket) => {
     }
   });
 
+  //Takes a client message and sends it to other clients, with arguments that suggest the scope of the message.
   socket.on('info/sendMessage', function(scope, msg) {
     switch (scope) {
+      //Regional message
       case "region":
         if (people[socket.id][3] == null) {
           socket.emit("info/message", "[INFO]", "You cannot send a table message when you are not at a table");
@@ -153,17 +197,19 @@ io.on('connection', (socket) => {
         }
         io.emit("info/regionMessage", people[socket.id][3], people[socket.id][0], msg);
         break;
-        case "global":
+      //Global
+      case "global":
           io.emit("info/message", people[socket.id][0], msg);
-        break;
-        default:
+      break;
+      //Invalid
+      default:
           socket.emit("info/message", "[INFO]", "Invalid message scope.");
     }
   });
 
 });
 
-//setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 
+//URL Interpretation
 app.get('/', (req, res) => res.render('pages/app.ejs'));
 app.get('/credits', (req, res) => res.render('pages/credits'));
